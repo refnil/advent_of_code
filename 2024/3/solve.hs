e27e11f825b490ad8ad047a6237aed0a18ec0029
Martin Lavoie <broemartino@gmail.com>
2024-12-03 09:35:05 -0400

    Update day 3

--- 2024/3/solve.hs
+++ 2024/3/solve.hs
@@ -1,23 +1,39 @@
-import Data.List (find, inits, tails)
+{-# LANGUAGE ViewPatterns #-}
+import Data.List (find, inits, tails, stripPrefix)
 import Data.List.NonEmpty (fromList)
 import Data.Maybe (fromMaybe)
 import Data.Semigroup (sconcat)
 import Data.Char (isDigit)
 
+type Data = [Token]
+
+parseFile :: String -> Data
+parseFile = toTokens
+
+puzzle1 :: Data -> Int
+puzzle1  = sum . eval 
+
+puzzle2 :: Data -> Int
+puzzle2 = sum . eval2
+
+main = do
+  content <- readFile "input"
+  let parsed = parseFile content
+
+  print (puzzle1 parsed)
+  print (puzzle2 parsed)
 
 data Token = Mul | OpenParens | CloseParens | Comma | Do | Dont | Num Int | Invalid
   deriving (Show, Eq)
 
-type Data = String
+when :: String -> String -> Maybe String
+when = stripPrefix
 
-
-
-
-toTokens :: Data -> [Token]
+toTokens :: String -> Data
 toTokens [] = []
-toTokens ('d':'o':'(':')':xs) = Do : toTokens xs
-toTokens ('d':'o':'n':'\'':'t':'(':')':xs) = Dont : toTokens xs
-toTokens ('m':'u':'l':xs) = Mul : toTokens xs
+toTokens (when "do()" -> Just rest) = Do : toTokens rest
+toTokens (when "don't()" -> Just rest) = Dont : toTokens rest
+toTokens (when "mul" -> Just rest) = Mul : toTokens rest
 toTokens (',':xs) = Comma : toTokens xs
 toTokens ('(':xs) = OpenParens : toTokens xs
 toTokens (')':xs) = CloseParens : toTokens xs
@@ -27,38 +43,18 @@ toTokens ss =
     [] -> Invalid : toTokens (tail rest)
     _ -> Num (read digits) : toTokens rest
 
-
-
-eval :: [Token] -> [Int]
+eval :: Data -> [Int]
 eval (Mul:OpenParens:(Num a):Comma: (Num b):CloseParens:xs) = a * b: eval xs
 eval (t:ts) =  eval ts
 eval [] = []
 
-eval2 :: [Token] -> [Int]
-eval2 (Mul:OpenParens:(Num a):Comma: (Num b):CloseParens:xs) = a * b: eval2 xs
+eval2 :: Data -> [Int]
+eval2 (Mul:OpenParens:(Num a):Comma:(Num b):CloseParens:xs) = a * b: eval2 xs
 eval2 (Dont:ts) = eval2Dont ts
 eval2 (t:ts) =  eval2 ts
 eval2 [] = []
 
-eval2Dont :: [Token] -> [Int]
+eval2Dont :: Data -> [Int]
 eval2Dont (Do:ts) = eval2 ts
 eval2Dont (_:ts) = eval2Dont ts
 eval2Dont [] = []
-
-parseFile :: String -> Data
-parseFile = id
-
-
-puzzle1 :: Data -> Int
-puzzle1 lines = sum . eval $ toTokens lines
-
-
-puzzle2 :: Data -> Int
-puzzle2 lines = sum . eval2 $ toTokens lines
-
-main = do
-  content <- readFile "input"
-  let parsed = parseFile content
-
-  print (puzzle1 parsed)
-  print (puzzle2 parsed)
